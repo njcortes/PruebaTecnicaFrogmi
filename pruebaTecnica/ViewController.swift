f6ef5668be9efc99dfd9287e8fb95602e72006af
Nestor <nj0588@gmail.com>
2022-01-02 11:10:29 -0300

    Initial Commit

---
+++ pruebaTecnica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pruebaTecnica
+//
+//  Created by Nestor Cortés on 02-01-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
